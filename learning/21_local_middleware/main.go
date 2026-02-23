a08d2b0a858249a6cd56762bfcbe0b1c2cf72bba
wzp <1239263644@qq.com>
2026-02-23 11:28:35 +0800

    21: 添加 gin.HandlerFunc 与 func 关系说明

--- learning/21_local_middleware/main.go
+++ learning/21_local_middleware/main.go
@@ -14,12 +14,12 @@ import (
 )
 
 // Logger 返回 gin.HandlerFunc，作为中间件仅对挂载它的路由生效
-/*gin.HandlerFunc 是 Gin 对 func(*gin.Context) 的类型别名：  
- • 作为路由处理函数：r.GET("/path", func(c *gin.Context) { ... })  
- • 作为中间件：r.Use(Logger()) 或 r.GET("/path", Logger(), handler)*/
+/*gin.HandlerFunc 是 Gin 对 func(*gin.Context) 的类型别名：
+   type HandlerFunc func(*gin.Context)
+   func(c *gin.Context){...} 的类型就是 HandlerFunc，可直接用于路由/中间件*/
 
 func Logger() gin.HandlerFunc {
-	return func(c *gin.Context) {
+	return func(c *gin.Context) { // 匿名函数类型 = gin.HandlerFunc
 		start := time.Now()
 		path := c.Request.URL.Path
 		clientIP := c.ClientIP()
